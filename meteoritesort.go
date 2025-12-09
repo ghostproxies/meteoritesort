26796653be9cc4336e367596094f9865b97bd0e3
William Stafford Parsons <wileylooper@gmail.com>
2025-12-09 11:57:50 -0800

    Added the Go implementation.

---
+++ meteoritesort.go
@@ -0,0 +1,44 @@
+// Copyright 2025 William Stafford Parsons
+
+func meteoriteSort(elements []int) {
+  var element int
+  gap := len(elements)
+  var i int
+  var j int
+
+  for gap > 15 {
+    gap = ((gap >> 2) - (gap >> 4)) + (gap >> 3)
+    i = gap
+
+    for i < len(elements) {
+      element = elements[i]
+      j = i
+
+      for j >= gap && elements[j - gap] > element {
+        elements[j] = elements[j - gap]
+        j -= gap
+      }
+
+      elements[j] = element
+      i++
+    }
+  }
+
+  i = 1
+  gap = 0
+
+  for i < len(elements) {
+    element = elements[i]
+    j = i
+
+    for j > 0 && elements[gap] > element {
+      elements[j] = elements[gap]
+      j = gap
+      gap--
+    }
+
+    elements[j] = element
+    gap = i
+    i++
+  }
+}
