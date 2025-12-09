f69c309a5f299292437d243cfe83c3bf306fa995
William Stafford Parsons <wileylooper@gmail.com>
2025-12-09 12:00:28 -0800

    Added the C implementation.

---
+++ meteoritesort.c
@@ -0,0 +1,52 @@
+// Copyright 2025 William Stafford Parsons
+
+#include <stddef.h>
+
+void meteoritesort(int *elements, size_t elements_length) {
+  int element;
+  size_t gap = elements_length;
+  size_t i;
+  size_t j;
+
+  while (gap > 15) {
+    gap = ((gap >> 2) - (gap >> 4)) + (gap >> 3);
+    i = gap;
+
+    while (i < elements_length) {
+      element = elements[i];
+      j = i;
+
+      while (
+        j >= gap &&
+        elements[j - gap] > element
+      ) {
+        elements[j] = elements[j - gap];
+        j -= gap;
+      }
+
+      elements[j] = element;
+      i++;
+    }
+  }
+
+  i = 1;
+  gap = 0;
+
+  while (i < elements_length) {
+    element = elements[i];
+    j = i;
+
+    while (
+      j > 0 &&
+      elements[gap] > element
+    ) {
+      elements[j] = elements[gap];
+      j = gap;
+      gap--;
+    }
+
+    elements[j] = element;
+    gap = i;
+    i++;
+  }
+}
